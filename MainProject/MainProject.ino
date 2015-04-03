a3c0a002c53f3c08dbb84d57c3a8435132c3c8ef
Robert Belter <robbotorigami@gmail.com>
2015-04-03 12:22:20 -0600

    Added Comments and Status light

--- MainProject/MainProject.ino
+++ MainProject/MainProject.ino
@@ -1,28 +1,28 @@
 /*
-  Software serial multple serial test
+  Rad Heights - Main Build
+  Arapahoe Community College
+  Created by Robert Belter
  
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
+Initializes sensors and logs to SD card
  
  The circuit: 
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
+ * Geiger counter on pin 9
+ * SD card through SPI
+ * Tempurature sensor on i2c
+ * Pressure sensor on i2c
+ * Heater on pin 6
+ * RGB Status LED on pins 2,3,4
  
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts, 
- so only the following can be used for RX: 
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
- 
- Not all pins on the Leonardo support change interrupts, 
- so only the following can be used for RX: 
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
- 
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
- 
- This example code is in the public domain.
+ Ignore below:
+ Status Config:
+ On turn on, the following events can happen in this order:
+ Status RED - 3 seconds - Power on
+   Status RED (blinking) - 5 seconds - SD card failiure
+ Status BLUE - 1 second - SD Finish
+   Status BLUE (blinking) - 5 seconds - Tempurature failiure
+   Status YELLOW (blinking) - 5 seconds - Pressure failiure
+ Status RED - forever - startup complete with errors
+ Status GREEN - forever - successful startup.
  
  */
  
@@ -37,8 +37,11 @@
 #include <SoftwareSerial.h>
 #include <MS5803_I2C.h>
 
+//Tempurature sensor constants
 #define tempRegister B00000000
 #define tempAddress B1001000
+
+//Pressure sensor constants
 #define pressAddress 0x77
 #define PROM1 B10100010
 #define PROM2 B10100100
@@ -51,13 +54,28 @@
 #define ADCREAD 0x00
 #define PRESRESET 0x1E
 
+//Heater Pin
+#define HEATERPIN 6
 
+//LED PIN Defines
+#define REDPIN 2
+#define GREENPIN 3
+#define BLUEPIN 4
+
+//Codes for display output
+#define POWERON 0
+#define SDFAILURE 1
+#define DONESUCCESS 6
+
+//Information for the sd card
 SdFat sd;
 SdFile DataFile;
 SdFile SensorFile;
 
+//Geiger counter connected on pin 9
 SoftwareSerial mySerial(9, 8); // RX, TX
 
+//Vars for reading from sensors
 byte msb, lsb;
 
 float temperature_c, temperature_f;
@@ -66,23 +84,41 @@ MS5803 sensor(ADDRESS_LOW);
 
 double base_altitude = 1655.0;
 
+//Vars to store data file names
 char fileName[8];
 char sensorFileName[8];
 
 void setup()  
 {
+  //Setup the pins for the led
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+  
+  displayStatus(POWERON);
+  
+  //Set up the heater
+  pinMode(HEATERPIN, OUTPUT)
   
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   pinMode(10, OUTPUT);
   digitalWrite(10,HIGH);
-  while(!sd.begin(10, SPI_HALF_SPEED)){}
-  Serial.println("sd initialized");
+  int sdStatus = SDFAILURE;
+  //Try 10 times to init sd card
+  for(int i = 0; i<10; i++)
+    if(sd.begin(10, SPI_HALF_SPEED)){
+      sdStatus = DONESUCCESS;
+      break;
+    }
+  displayStatus(sdStatus);
+  
+  //Initialize files
   int i = 0;
-  sprintf(fileName, "g%i.txt", i);
+  sprintf(fileName, "g%i.csv", i);
   while(sd.exists(fileName)){
     i++;
-    sprintf(fileName, "g%i.txt", i);
+    sprintf(fileName, "g%i.csv", i);
   }
   DataFile.open(fileName, O_RDWR | O_CREAT | O_AT_END);
   
@@ -105,27 +141,43 @@ void setup()
   sensor.reset();
   sensor.begin();  
   pressure_baseline = sensor.getPressure(ADC_4096);
+  
 }
 
 void loop() // run over and over
 {
-  if (mySerial.available()){
+  //If the geiger counter detected an issue, log the time
+  while (mySerial.available()){
     DataFile.open(fileName,O_RDWR | O_CREAT | O_AT_END);
     DataFile.println(millis());
     mySerial.read();
     DataFile.close();  
   } 
   
+  //Gather all sensor data and print it
   SensorFile.open(sensorFileName,O_RDWR | O_CREAT | O_AT_END);
   SensorFile.print(millis());
   SensorFile.print(", ");
-  SensorFile.print(readTemp());
+  float temp = readTemp();
+  SensorFile.print(temp);
   SensorFile.print(", ");
   pressure_abs = sensor.getPressure(ADC_4096);
   SensorFile.print(pressure_abs);
   SensorFile.print(", ");
   SensorFile.println(altitude(pressure_abs, pressure_baseline));
   SensorFile.close();
+  
+  //Print data to serial (can be disabled)
+  printSerial(temp, pressure_abs);
+  
+  //If the temperature is low, turn on the heater
+  if(temp < 10)
+    digitalWrite(HEATERPIN, HIGH);
+  
+  //If the temperature is high enough turn of the heater
+  if(temp > 15)
+    digitalWrite(HEATERPIN, LOW);
+    
   delay(100);
 }
 
@@ -148,3 +200,37 @@ double altitude(double P, double P0)
 {
 	return(44330.0*(1-pow(P/P0,1/5.255)));
 }
+
+void displayStatus(int toDisp){
+  switch(toDisp){
+    case POWERON:
+      digitalWrite(REDPIN, LOW);
+      digitalWrite(GREENPIN, LOW);
+      digitalWrite(BLUEPIN, HIGH);
+      Serial.println("-------------------------POWER ON--------------------------");
+      break;
+    case SDFAILURE:
+      digitalWrite(REDPIN, HIGH);
+      digitalWrite(GREENPIN, LOW);
+      digitalWrite(BLUEPIN, LOW);
+      Serial.println("!!!SD CARD FAILURE!!!");
+      break;
+    case DONESUCCESS:
+      digitalWrite(REDPIN, LOW);
+      digitalWrite(GREENPIN, HIGH);
+      digitalWrite(BLUEPIN, LOW);
+      Serial.println("Initialized successfully");
+  }  
+}
+
+void printSerial(float temp, float pressure){
+  Serial.print("The tempurature is:");
+  Serial.print(temp);
+  Serial.print("C, The pressure is:");
+  Serial.print(pressure);
+  Serial.print("mbar, Change in alititude:");
+  Serial.print(altitude(pressure_abs, pressure_baseline));
+  Serial.println("m");
+  Serial.println();
+}
+  
